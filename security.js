974f844e435761e74dc93c04cdf76c97538449e3
Kalzz Protocol <kallzzdev@gmail.com>
2026-03-07 22:59:56 +0700

    Update security.js

--- security.js
+++ security.js
@@ -1,47 +1,39 @@
-document.addEventListener("contextmenu", e => e.preventDefault());
+document.addEventListener('contextmenu', e => e.preventDefault());
 
-document.addEventListener("keydown", function(e) {
-
+document.onkeydown = function(e) {
     if (
-        e.key === "F12" ||
-        (e.ctrlKey && e.shiftKey && ["I","J","C"].includes(e.key)) ||
-        (e.ctrlKey && e.key === "U")
+        e.keyCode == 123 || // F12
+        (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'C'.charCodeAt(0))) || // Ctrl+Shift+I/J/C
+        (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) // Ctrl+U
     ) {
-        e.preventDefault();
-        alert("Inspect tidak diizinkan!");
+        return false;
     }
+};
 
-});
-
-let devtoolsOpen = false;
-setInterval(function () {
-
-    const widthThreshold = window.outerWidth - window.innerWidth > 160;
-    const heightThreshold = window.outerHeight - window.innerHeight > 160;
-
-    if (widthThreshold || heightThreshold) {
-        if (!devtoolsOpen) {
-            devtoolsOpen = true;
-            alert("DevTools terdeteksi!");
-            location.reload();
-        }
-
-    } else {
-        devtoolsOpen = false;
-    }
-
-}, 1000);
-
-setInterval(function () {
-    debugger;
-}, 200);
-
-(function () {
-    const element = new Image();
-    Object.defineProperty(element, 'id', {
-        get: function () {
-            location.reload();
-        }
-    });
-    console.log(element);
+(function() {
+    setInterval(function() {
+        Function('debugger')();
+    }, 50);
 })();
+
+const devtools = {
+    isOpen: false,
+    orientation: undefined
+};
+const threshold = 160;
+setInterval(() => {
+    const widthThreshold = window.outerWidth - window.innerWidth > threshold;
+    const heightThreshold = window.outerHeight - window.innerHeight > threshold;
+    
+    if (widthThreshold || heightThreshold) {
+        if (!devtools.isOpen) {
+            window.location.reload(); 
+        }
+        devtools.isOpen = true;
+    } else {
+        devtools.isOpen = false;
+    }
+}, 500);
+
+console.log("%cSTOP!", "color: red; font-size: 50px; font-weight: bold;");
+console.log("%cFitur ini dilindungi oleh sistem keamanan Indraa Store.", "font-size: 18px; color: yellow;");
